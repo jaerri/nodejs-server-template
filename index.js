e5023d95476319cdd75c5d001f72aac17c44a897
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2021-03-31 19:58:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const fs = require("fs");
 const url = require("url");
 
 http.createServer((req, res) => {
-    var pathname = url.parse(req.url).pathname;
-
+    let pathname = url.parse(req.url);
 }).listen(process.env.PORT || 8080, null, null, () => console.log("ae"));
