df8a739fc9722d0a2b703fe806ad9ed30d990282
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2021-04-23 20:40:22 +0700

    improvements
    rework index.js, added meta tags to html, added favicon.ico assets

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ const fs = require("fs");
 const url = require("url");
 const path = require("path");
 
+const mimeTypes = require("./dev/mimeTypes.json");
 const port = 8080;
-const mimeTypes = require("./mimeTypes.json");
+const clientFilesPath = "./data";
+const htmlFileName = "/index.html";
 
-http.createServer((req, res) => {
-    let baseURL = 'http://' + req.headers.host + '/';
-    let parsedURL = new url.URL(req.url, baseURL);
+http.createServer((req, res) => {;
+    switch (req.method) {
+        case "GET":
+            let baseURL = 'http://' + req.headers.host + '/';
+            let parsedURL = new url.URL(req.url, baseURL);
 
-    let filePath = "." + "/dev" + parsedURL.pathname;
-    if (filePath == './dev/') filePath = './dev/index.html';
-    let ext = path.extname(filePath).toLowerCase();
-    let contentType = mimeTypes[ext];
+            let filePath = clientFilesPath + parsedURL.pathname;
+            if (parsedURL.pathname == "/") filePath = clientFilesPath + htmlFileName;
+            let ext = path.extname(filePath).toLowerCase();
+            let contentType = mimeTypes[ext];
 
-    if (!req.method == "GET") return res.end();
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            console.log(error);
-            if (error.code == 'ENOENT') res.writeHead(404);
-            else res.writeHead(500);  
-            res.end();
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
+            fs.readFile(filePath, (error, content) => {
+                if (error) {
+                    console.log(error);
+                    if (error.code == 'ENOENT') res.writeHead(404);
+                    else res.writeHead(500);  
+                    res.end();
+                } else {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                }
+            });
+            break;
+    }
 }).listen(process.env.PORT || port, null, null, () => console.log("ae"));
